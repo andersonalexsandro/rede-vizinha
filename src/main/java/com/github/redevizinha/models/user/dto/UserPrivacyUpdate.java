e1fd3b32b4d0fd9ca3f607b29885824243ddc90c
andersonalexsandro <anderson.silva.04@Outlook.com>
2025-09-30 21:16:58 -0300

    refact: dto record to class

--- src/main/java/com/github/redevizinha/models/user/dto/UserPrivacyUpdate.java
+++ src/main/java/com/github/redevizinha/models/user/dto/UserPrivacyUpdate.java
@@ -1,5 +1,10 @@
 package com.github.redevizinha.models.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserPrivacyUpdate {
     private  boolean isPublicProfile;
 }
